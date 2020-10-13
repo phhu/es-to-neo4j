c4e120b304f66869a548404f52a6b071f0df6610
phhu <phhu.git@gmail.com>
2020-10-13 12:06:27 +0100

    added components to releases

--- README.md
+++ README.md
@@ -10,4 +10,5 @@ Then http://localhost:3000/update/commits?from=now-7d&to=now etc
 curl "http://localhost:3000/update/commits?from=now-7d&to=now"
 
 
-curl "http://localhost:3000/update/releases?repo=SC_DB"
+curl "http://localhost:3000/update/releases"
+ 
--- spec/releases.js
+++ spec/releases.js
@@ -8,21 +8,31 @@ const components = {
 };
 
 module.exports = ({
-  from="now-1d",
+  from="now-1y",
   to="now",  
-  repo="SC_WFM",
+  //repo="SC_WFM",
 }={})=>({
   props: {
     index: 'artifactory',
     returnHits: true,
     type: 'doc',
-    query: `containedInRelease:* AND component:"${components[repo] || "*"}" AND repo:"T3-Passed-QA" AND NOT extension:jar `,   //Uploaded    :"performance management"
+    reposByComponent: {
+      "WFM Common": "SC_WFM",
+      "performance management": "SC_SCORECARDS",
+      "Framework Database BPMAINDB": "SC_DB",
+      "Framework Foundation": "SC_FOUNDATION",
+      "Framework Integration Server": "SC_INTEG_COMMON",
+      "Backoffice Operations": "SC_OPS",
+      "Forecasting and Scheduling": "SC_ALGO",
+      "Framework Reporting": "SC_REPORTING",
+    },
+    query: `containedInRelease:* AND repo:"T3-Passed-QA" AND NOT extension:jar AND date:[${from} TO ${to}]`,   //Uploaded    :"performance management"
     source: [ "repo", "date","product","component","version","kb","containedInRelease", "repo","lastUpdated" ],
   },
   query: `
     MATCH (t:Tag)
     WHERE
-      t.repo = '${repo}'
+      t.repo = $props.reposByComponent[hit._source.component] 
       AND t.name =~ '.*' + hit._source.version
     MERGE (x:Release {name: hit._source.containedInRelease})
     MERGE (k:KB {number: hit._source.kb})
@@ -36,6 +46,9 @@ module.exports = ({
 });
 
 /*
+AND component:"${components[repo] || "*"}"
+'${repo}'
+
    SC_SCORECARDS
     MATCH (t:Tag)
     WHERE
